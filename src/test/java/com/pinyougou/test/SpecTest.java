4368ebce9c9356ecf650f8b5970942056c545380
Kristopher <1142264663@qq.com>
2019-07-26 21:58:33 +0800

    提交

---
+++ src/test/java/com/pinyougou/test/SpecTest.java
@@ -0,0 +1,33 @@
+package com.pinyougou.test;
+
+import com.pinyougou.dao.CustomerDao;
+import com.pinyougou.domain.Customer;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import javax.persistence.criteria.*;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = "classpath:applicationContext.xml")
+public class SpecTest {
+    @Autowired
+    private CustomerDao customerDao;
+
+    @Test
+    public void testSpec() {
+        Specification<Customer> spec = new Specification<Customer>() {
+            @Override
+            public Predicate toPredicate(Root<Customer> root, CriteriaQuery<?> query, CriteriaBuilder cb) {
+                Path<Object> custName = root.get("custName");
+                Predicate predicate = cb.equal(custName, "gg");
+                return predicate;
+            }
+        };
+        Customer customer = customerDao.findOne(spec);
+        System.out.println(customer);
+    }
+}
